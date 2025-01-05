74675594ec8482be649ae96af7bb1a16fd0a25aa
mpoyraz <mchdardc@gmail.com>
2025-01-06 01:27:13 +0300

    Added : Dal katmanına ait IGeneric ve Generic eklendi. Metotların imzası ve govdeleri yazıldı.

---
+++ TraversalCoreMY.DAL/Abstract/IGenericDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TraversalCoreMY.Entity.Concrete;
+
+namespace TraversalCoreMY.DAL.Abstract
+{
+    public interface IGenericDal<T> where T : BaseEntity, new()
+    {
+        Task<List<T>> GetAllDALAsync();
+        Task<List<T>> GetAllDALAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetByIdDALAsync(int id);
+        Task<T> GetSingleDALAsync(Expression<Func<T, bool>> filter);
+        Task<T> CreateDALAsync(T entity);
+        Task<T> UpdateDALAsync(T entity);
+        Task DeleteDALAsync(T entity);
+        Task<int> CountDALAsync();
+        Task<bool> ExistsDALAsync(Expression<Func<T, bool>> filter);
+    }
+}
---
+++ TraversalCoreMY.DAL/Repository/GenericDal.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TraversalCoreMY.DAL.Abstract;
+using TraversalCoreMY.DAL.Context;
+using TraversalCoreMY.Entity.Concrete;
+
+namespace TraversalCoreMY.DAL.Repository
+{
+    public class GenericDal<T> : IGenericDal<T> where T : BaseEntity, new()
+    {
+        private readonly TraversalCoreMYContext _context;
+
+        public GenericDal(TraversalCoreMYContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CountDALAsync()
+        {
+            try
+            {
+                return await _context.Set<T>().CountAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while counting entities.", ex);
+            }
+        }
+
+        public async Task<T> CreateDALAsync(T entity)
+        {
+            try
+            {
+                var createEntity = await _context.Set<T>().AddAsync(entity).ConfigureAwait(false);
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+                return createEntity.Entity;
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while creating the entity.", ex);
+            }
+        }
+
+        public async Task DeleteDALAsync(T entity)
+        {
+            try
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while deleting the entity.", ex);
+            }
+        }
+
+        public async Task<bool> ExistsDALAsync(Expression<Func<T, bool>> filter)
+        {
+            try
+            {
+                return await _context.Set<T>().AnyAsync(filter).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while checking if the entity exists.", ex);
+            }
+        }
+
+        public async Task<List<T>> GetAllDALAsync()
+        {
+            try
+            {
+                return await _context.Set<T>().ToListAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while getting all entities.", ex);
+            }
+        }
+
+        public async Task<List<T>> GetAllDALAsync(Expression<Func<T, bool>> filter)
+        {
+            try
+            {
+                return await _context.Set<T>().Where(filter).ToListAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while getting filtered entities.", ex);
+            }
+        }
+
+        public async Task<T> GetByIdDALAsync(int id)
+        {
+            try
+            {
+                return await _context.Set<T>().FindAsync(id).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while getting the entity by id.", ex);
+            }
+        }
+
+        public async Task<T> GetSingleDALAsync(Expression<Func<T, bool>> filter)
+        {
+            try
+            {
+                return await _context.Set<T>().FirstOrDefaultAsync(filter).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while getting the single entity.", ex);
+            }
+        }
+
+        public async Task<T> UpdateDALAsync(T entity)
+        {
+            try
+            {
+                _context.Set<T>().Update(entity);
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                // Hataları loglayabilirsiniz
+                throw new Exception("An error occurred while updating the entity.", ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
